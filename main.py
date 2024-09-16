00b31c7f95c9a635e617af49807f94e98630985c
Elizaveta Tyshchenko <elizabeth131306@gmail.com>
2024-09-16 17:44:54 +0300

    Lates commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Test 3")
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+n = int(input("К-сть чисел Фібоначчі: "))
+fib_sequence = fibonacci(n)
+
+print(f"Перші {n} чисел Фібоначчі: {fib_sequence}")
